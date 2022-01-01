8e52a6accd8101bf6e545b223188f761d8c1b182
rizTaak <rizwansharif@hotmail.com>
2022-01-01 23:07:31 +0800

    minor log setup

--- src/main.rs
+++ src/main.rs
@@ -4,13 +4,20 @@ mod external;
 mod oxide;
 mod sandbox;
 
+use std::path::Path;
+
 use crate::oxide::app::Application;
 
 fn main() {
-    log4rs::init_file("log4rs.yml", Default::default()).unwrap();
+    if Path::new("log4rs.yml").exists() {
+        log4rs::init_file("log4rs.yml", Default::default()).unwrap();
+    } else {
+        println!("Skipping log setup as log4rs.yml not found.")
+    }
     oxide_info!("Starting Oxide");
     let mut app = sandbox::app::SandboxApp::new();
     app.run();
     oxide_info!("Stopping Oxide");
     app.close();
+    oxide_info!("Oxide Stopped");
 }
